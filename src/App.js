1c45ef9bd8867c53631ae91304bc1ff9b2d49d35
Shikhar-ss <shikhar.sharma.ss.24@gmail.com>
2021-02-23 17:26:40 +0530

    added getList functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { useState,useEffect } from "react";
+import {getList} from "./services/list";
 
 function App() {
+
+  const [list,setList] = useState([])
+
+  useEffect(()=>{
+    let mounted = true;
+    getList()
+    .then(items => {
+      if(mounted){
+        setList(items)
+      }
+    })
+    return () => mounted =false
+  }, [] )
   return (
-    <div className="App">
-      <h1>UseEffect</h1>
+    <div className="wrapper">
+      <ul>
+      {list.map(item => <li key={item.item}>{item.item}</li>)}
+      </ul>
     </div>
   );
 }
