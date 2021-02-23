624caa8b1a72efc9024cb9a1d8c8f6afd214ae44
Shikhar-ss <shikhar.sharma.ss.24@gmail.com>
2021-02-23 23:07:18 +0530

    set data in local API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import './App.css';
-import { useState,useEffect } from "react";
-import {getList} from "./services/list";
+import { useState, useEffect } from "react";
+import { getList, setItem } from "./services/list";
 
 function App() {
 
-  const [list,setList] = useState([])
-
-  useEffect(()=>{
+  const [list, setList] = useState([])
+  const [itemInput, setItemInput] = useState('')
+  // const [clearItem, resetItemInput] = useState()
+  useEffect(() => {
     let mounted = true;
     getList()
-    .then(items => {
-      if(mounted){
-        setList(items)
-      }
-    })
-    return () => mounted =false
-  }, [] )
+      .then(items => {
+        if (mounted) {
+          setList(items)
+        }
+      })
+    return () => mounted = false
+  }, [itemInput])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setItem(itemInput).then(() => {
+      setItemInput('');
+    });
+  }
+
   return (
     <div className="wrapper">
       <ul>
-      {list.map(item => <li key={item.item}>{item.item}</li>)}
+        {list.map(item => <li key={item.item}>{item.item}</li>)}
       </ul>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>NEW ITEM: </p>
+          <input type='text' onChange={e => setItemInput(e.target.value)} value={itemInput} />
+        </label>
+        <button type='submit' >SUBMIT</button>
+      </form>
     </div>
   );
 }
--- src/services/list.js
+++ src/services/list.js
@@ -3,3 +3,13 @@ export function getList() {
         .then(data => data.json())
 }
 
+export function setItem(item){
+    return fetch('http://localhost:3333/list',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({item})
+    })
+    .then(data => data.json())
+}
